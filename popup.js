78918964001102c06cd5a52130fe1f274e514267
John <106767410+Jon1007@users.noreply.github.com>
2023-01-08 02:53:33 +0100

    new

---
+++ popup.js
@@ -0,0 +1,10 @@
+const timeElement = document.getElementById("time");
+const currentTime = new Date().toLocaleTimeString()
+
+timeElement.textContent = `Time is ${currentTime}`;
+
+chrome.action.setBadgeText({
+	text:"TIME"
+},() => {
+	console.log("Finished Badge Text")
+} )
