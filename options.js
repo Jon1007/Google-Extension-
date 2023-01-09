a7326286aed590c362fe20181740b8c21f9b8767
John <106767410+Jon1007@users.noreply.github.com>
2023-01-09 07:38:33 +0100

    last commit

--- options.js
+++ options.js
@@ -1,6 +1,14 @@
-const name  = document.getElementById("name")
-const saveBtn = document.getElementById("btn")
+const inputVal = document.getElementById("inputVal");
+const inputNam = document.getElementById("inputNum");
+const saveBtn = document.getElementById("btns");
 
 saveBtn.addEventListener("click", () => {
-	console.log(name.value)
-})
+  const name = inputVal.value;
+  const notificationTime = inputNam.value;
+  chrome.storage.sync.set({ name, notificationTime });
+});
+
+chrome.storage.sync.get(["name", "notificationTime"], (result) => {
+  inputVal.value = result.name;
+  inputNam.value = result.notificationTime ?? 1000;
+});
--- popup.js
+++ popup.js
@@ -1,11 +1,34 @@
 const timeElement = document.getElementById("time");
-const currentTime = new Date().toLocaleTimeString()
+const timerElement = document.getElementById("timer");
+const nameElement = document.getElementById("name");
+const startBtn = document.getElementById("start");
+const stoptBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
 
-timeElement.textContent = `Time is ${currentTime}`;
+function updateTimeElements() {
+  chrome.storage.local.get(["timer"], (result) => {
+    const time = result.timer ?? 0;
+    timerElement.textContent = `The timer is at : ${time} seconds`;
+  });
+  const currentTime = new Date().toLocaleTimeString();
+  timeElement.textContent = `Time is ${currentTime}`;
+}
+updateTimeElements();
+setInterval(updateTimeElements, 1000);
 
-chrome.action.setBadgeText({
-	text:"TIME"
-},() => {
-	console.log("Finished Badge Text")
-} )
+chrome.storage.sync.get(["name"], (result) => {
+  const name = result.name ?? "???";
+  nameElement.textContent = `Your name is ${name}`;
+});
 
+startBtn.addEventListener("click", () => {
+  chrome.storage.local.set({ isRunning: true });
+});
+
+stoptBtn.addEventListener("click", () => {
+  chrome.storage.local.set({ isRunning: false });
+});
+
+resetBtn.addEventListener("click", () => {
+  chrome.storage.local.set({ timer: 0, isRunning: false });
+});
